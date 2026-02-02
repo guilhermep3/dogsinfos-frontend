63eebcb8ac22a3510d3de30cbb39acb97e48a09e
Guilherme <145510603+Guilhermep7@users.noreply.github.com>
2026-02-02 14:35:35 -0300

    fix: update svg

--- src/components/svg/blueBg.tsx
+++ src/components/svg/blueBg.tsx
@@ -1,23 +1,31 @@
 export const BlueBg = () => {
-
   return (
-    <svg className="border shadow-md dark:border-slate-700 absolute -z-[1] rounded-2xl"
+    <svg
+      className="border shadow-md dark:border-slate-700 absolute -z-[1] rounded-2xl"
       viewBox="0 0 1006.72 503.36"
-      style={{width: '100%', height: '100%'}}
-      xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="none">
-        <rect className="bg" id="bg" x="0" y="0" width="1006.72" height="503.36" fill="#00ddfa"></rect>
-        <g transform="rotate(40 503.36 251.68)">
-          <path d="M -503.36 183.36 S -261.68 -44.64
+      width="100%" height="100%"
+      xmlns="http://www.w3.org/2000/svg"
+      preserveAspectRatio="none"
+    >
+      <rect x="0" y="0" width="1006.72" height="503.36" fill="#00ddfa" />
+      <g transform="rotate(40 503.36 251.68)">
+        <path
+          d="M -503.36 183.36 S -261.68 -44.64
             0.00 183.36 22.36 -58.64
             503.36 183.36 745.04 116.36
             1006.72 183.36 1248.40 113.36
-            1510.08 183.36 h 110 V 1103.3600000000001 H -503.36 Z" fill="#1447E6 "></path>
-          <path d="M -503.36 345.00 S -305.00 182.50
+            1510.08 183.36 h 110 V 1103.36 H -503.36 Z"
+          fill="#1447E6"
+        />
+        <path
+          d="M -503.36 345.00 S -305.00 182.50
             0.00 345.00 227.36 182.50
             503.36 345.00 745.04 182.50
             1006.72 345.00 1248.40 182.50
-            1510.08 345.00 h 110 V -600 H -503.36 Z" fill="#1C398E "></path>
-          </g>
-      </svg>
+            1510.08 345.00 h 110 V -600 H -503.36 Z"
+          fill="#1C398E"
+        />
+      </g>
+    </svg>
   )
 }
--- src/components/svg/grayBg.tsx
+++ src/components/svg/grayBg.tsx
@@ -1,22 +1,30 @@
 export const GrayBg = () => {
-
   return (
-    <svg className="border shadow-md dark:border-slate-700 absolute -z-[1] rounded-2xl"
+    <svg
+      className="border shadow-md dark:border-slate-700 absolute -z-[1] rounded-2xl"
       viewBox="0 0 1006.72 503.36"
-      style={{ width: '100%', height: '100%' }}
-      xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="none">
-      <rect className="bg" id="bg" x="0" y="0" width="1006.72" height="503.36" fill="#ffffff"></rect>
+      width="100%" height="100%"
+      xmlns="http://www.w3.org/2000/svg"
+      preserveAspectRatio="none"
+    >
+      <rect x="0" y="0" width="1006.72" height="503.36" fill="#ffffff" />
       <g transform="rotate(214 503.36 251.68)">
-        <path d="M -503.36 183.36 S -261.68 126.36
-          0.00 183.36 17.36 183.36
-          503.36 183.36 525.72 77.36
-          1006.72 183.36 1248.40 135.36
-          1510.08 183.36 h 110 V 1103.3600000000001 H -503.36 Z" fill="#c9d3e6"></path>
-        <path d="M -503.36 345.00 S -480.00 48.00
-          0.00 345.00 21.36 182.50
-          503.36 345.00 520.72 182.50
-          1006.72 345.00 1248.40 81.00
-          1510.08 345.00 h 110 V -600 H -503.36 Z" fill="#b9c4d8"></path>
+        <path
+          d="M -503.36 183.36 S -261.68 126.36
+            0.00 183.36 17.36 183.36
+            503.36 183.36 525.72 77.36
+            1006.72 183.36 1248.40 135.36
+            1510.08 183.36 h 110 V 1103.36 H -503.36 Z"
+          fill="#c9d3e6"
+        />
+        <path
+          d="M -503.36 345.00 S -480.00 48.00
+            0.00 345.00 21.36 182.50
+            503.36 345.00 520.72 182.50
+            1006.72 345.00 1248.40 81.00
+            1510.08 345.00 h 110 V -600 H -503.36 Z"
+          fill="#b9c4d8"
+        />
       </g>
     </svg>
   )
